bf1445a979f4469a839188af934a6e864c64ef7a
harmoniousmoss <syaefulbahri@protonmail.com>
2024-10-20 12:04:19 +0700

    Footer comp

---
+++ app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <footer className="bg-muted text-muted-foreground p-4 text-center">
+      <p>&copy; 2024 Remote DevJobs. All rights reserved.</p>
+    </footer>
+  );
+}
